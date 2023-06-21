e6309a7667e81b0c6e24298cbde2d3e0a1d72979
umeme <umeme1011@gmail.com>
2023-06-21 17:05:07 +0900

    Initial Commit

---
+++ OshiDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OshiDiary
+//
+//  Created by umeme on 2023/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
