8f517a783911af27539885715129774f73db2e0c
umeme <umeme1011@gmail.com>
2023-07-11 09:07:14 +0900

    日記検索完了

--- OshiDiary/ViewController/DiarySearchViewController.swift
+++ OshiDiary/ViewController/DiarySearchViewController.swift
@@ -13,17 +13,24 @@ class DiarySearchViewController: UIViewController {
     @IBOutlet var baseView: UIView!
     @IBOutlet weak var searchTF: UITextField!
     @IBOutlet weak var backgroundIV: UIImageView!
+    @IBOutlet weak var listTV: UITableView!
     
     var myUD: MyUserDefaults!
     var oshiRealm: Realm!
-    
+    var oshiId: Int!
+    var diaryDic: Dictionary = Dictionary<String, [Diary]>()
+    var keyArray: [String] = [String]()
+    var diary: Diary!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.searchTF.delegate = self
+        searchTF.delegate = self
+        listTV.delegate = self
+        listTV.dataSource = self
         
         myUD = MyUserDefaults.init()
-
+        oshiId = myUD.getOshiId()
         self.changeVisual()
         
         // 枠線
@@ -50,6 +57,19 @@ class DiarySearchViewController: UIViewController {
         // ランダムに背景画像を設定
         backgroundIV.image = CommonMethod.roadBackgroundImage(oshiId: myUD.getOshiId()).randomElement()
     }
+    
+    /**
+     データ渡し
+     */
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // 編集画面にデータを渡す
+        if segue.identifier == "toDiaryEdit" {
+            let vc: DiaryEditViewController = segue.destination as! DiaryEditViewController
+            vc.diary = diary
+            vc.isNew = false
+            vc.selectedDate = diary.date
+        }
+    }
 }
 
 extension DiarySearchViewController: UITextFieldDelegate {
@@ -57,6 +77,8 @@ extension DiarySearchViewController: UITextFieldDelegate {
      キーボードEnter押下
      */
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // diaryDic初期化
+        diaryDic.removeAll()
         
         // 検索文字列を取得、空白で分割
         var text: String = textField.text ?? ""
@@ -72,20 +94,188 @@ extension DiarySearchViewController: UITextFieldDelegate {
         }
         
         // oshiRealm生成
-        oshiRealm = CommonMethod.createOshiRealm(oshiId: myUD.getOshiId())
+        oshiRealm = CommonMethod.createOshiRealm(oshiId: oshiId)
         // 検索
         var predicates: [NSPredicate] = [NSPredicate]()
         for text in textArray {
             let predicate = NSPredicate(format: "\(Diary.Types.title.rawValue) CONTAINS[c] %@ OR \(Diary.Types.content.rawValue) CONTAINS[c] %@", text.trimmingCharacters(in: .whitespaces), text.trimmingCharacters(in: .whitespaces))
             predicates.append(predicate)
         }
-        var result = oshiRealm.objects(Diary.self).filter(NSCompoundPredicate(andPredicateWithSubpredicates: predicates))
+        var diaries = oshiRealm.objects(Diary.self).filter(NSCompoundPredicate(andPredicateWithSubpredicates: predicates))
+            .sorted(byKeyPath: Diary.Types.date.rawValue, ascending: true)
 
-        // 2023/07/11　結果を年月でわけてDicに格納、リスト表示
-        
+        // 結果を年月で分類しDicに格納
+        if !diaries.isEmpty {
+            var tmpMonth = ""
+            var diaryArray: [Diary] = [Diary]()
+            for diary in diaries {
+                if tmpMonth == diary.ymString {
+                    diaryArray.append(diary)
+                } else {
+                    if !diaryArray.isEmpty {
+                        diaryDic[tmpMonth] = diaryArray
+                        diaryArray.removeAll()
+                    }
+                    diaryArray.append(diary)
+                }
+                tmpMonth = diary.ymString
+            }
+            diaryDic[tmpMonth] = diaryArray
+        }
+        // キー（日付）配列
+        keyArray = [String](diaryDic.keys)
+        // キーをソート（年月降順）
+        keyArray.sort { $0 > $1 }
+        // TVリロード
+        listTV.reloadData()
+
         // キーボードを閉じる
         textField.resignFirstResponder()
         return true
+    }
+}
+
+extension DiarySearchViewController: UITableViewDelegate, UITableViewDataSource {
+    /**
+     セクション数
+     */
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return keyArray.count
+    }
+    
+    /**
+     セクション内容
+     */
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view: UIView = UIView()
+        let label: UILabel = UILabel()
+        
+        let cnt = diaryDic[keyArray[section]]?.count
+        label.text = "\(keyArray[section])  \(cnt ?? 0)件"
+
+        // Viewデザイン
+        let screenWidth:CGFloat = listTV.frame.size.width
+        view.layer.borderColor = UIColor.white.cgColor
+        view.layer.borderWidth = 0.0
+        view.frame = CGRect(x:0, y:0, width:screenWidth, height:25)
+        view.backgroundColor = Const.Color().getImageColor(cd: myUD.getImageColorCd())
+        // labelデザイン
+        label.sizeToFit()
+        label.font = UIFont.systemFont(ofSize: 13)
+        label.frame = CGRect(x:10, y:0, width:screenWidth-10, height:25)
+        label.textColor = UIColor.darkGray
+        
+        view.addSubview(label)
+        
+        // セクションのビューに対応する番号を設定する
+        view.tag = section
+        
+        return view
+    }
+
+    /**
+     セクション高さ
+     */
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 25
+    }
+
+    /**
+     セル数
+     */
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return diaryDic[keyArray[section]]?.count ?? 0
+    }
+
+    /**
+     セル内容
+     */
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier:  "cell", for:indexPath as IndexPath)
+            as! ListTableViewCell
+
+        // キーをもとにDiaryを取得
+        let diaryArray: [Diary] = diaryDic[keyArray[indexPath.section]]!
+        
+        if indexPath.row < diaryArray.count {
+            let current = Calendar.current
+            cell.dateLbl.text = String(current.component(.day, from: diaryArray[indexPath.row].date))
+            cell.weekLbl.text = CommonMethod.weekFormatter(date: diaryArray[indexPath.row].date)
+            cell.timeLbl.text = CommonMethod.dateFormatter(date: diaryArray[indexPath.row].date, formattKind: Const.DateFormatt.HM)
+            cell.titleLbl.text = diaryArray[indexPath.row].title
+            cell.contentLbl.text = diaryArray[indexPath.row].content
+            cell.imageIV.image = CommonMethod.roadDiaryImage(oshiId: oshiId, diaryId: diaryArray[indexPath.row].id).first
+        }
+        
+        return cell
+    }
+
+    /**
+     セルがタップされた時の処理
+     */
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        // タップしたセルを取得
+        if let cell: UITableViewCell = listTV.cellForRow(at: indexPath) {
+            // 選択状態を解除
+            cell.isSelected = false
+        }
+        // 編集画面へ遷移
+        diary = diaryDic[keyArray[indexPath.section]]![indexPath.row]
+        performSegue(withIdentifier: "toDiaryEdit", sender: nil)
+    }
+
+    /**
+     右から左へスワイプ
+     */
+    func tableView(_ tableView: UITableView,
+                   trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let delete = UIContextualAction(style: .normal,
+                                            title: "削除",
+                                            handler: { (action: UIContextualAction, view: UIView, success :(Bool) -> Void) in
+            
+            let alert = UIAlertController(title: "", message: Const.Message.DELTE_CONFIRM_MSG, preferredStyle: .alert)
+            let cancel = UIAlertAction(title: "いいえ", style: .default, handler: { (action) -> Void in
+                // アラートを閉じる
+                alert.dismiss(animated: true)
+            })
+            let ok = UIAlertAction(title: "はい", style: .default, handler: { (action) -> Void in
+                var diaryArray: [Diary] = self.diaryDic[self.keyArray[indexPath.section]]!
+
+                // 日記画像ディレクトリ削除
+                let oshiIdStr: String = String(self.oshiId)
+                let diaryIdStr: String = String(diaryArray[indexPath.row].id)
+                let dirName: String = Const.File.OSHI_DIR_NAME + oshiIdStr
+                                    + "/" + Const.File.Diary.DIARY_DIR_NAME + diaryIdStr
+                CommonMethod.removeFile(name: dirName)
+                
+                // DB物理削除
+                if let diary: Diary = self.oshiRealm.objects(Diary.self)
+                    .filter("\(Diary.Types.id.rawValue) = %@", diaryArray[indexPath.row].id).first {
+                    
+                    do {
+                        try self.oshiRealm.write {
+                            self.oshiRealm.delete(diary)
+                        }
+                    } catch {
+                        print("削除失敗", error)
+                    }
+                    // 削除したデータをDicからも削除
+                    diaryArray.remove(at: indexPath.row)
+                    self.diaryDic[self.keyArray[indexPath.section]] = diaryArray
+                    self.listTV.reloadData()
+                }
+            })
+            alert.addAction(cancel)
+            alert.addAction(ok)
+            self.present(alert, animated: true, completion: nil)
+            
+            success(true)
+        })
+        delete.backgroundColor = UIColor.systemRed
+
+        return UISwipeActionsConfiguration(actions: [delete])
     }
 
 }
