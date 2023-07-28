49fd7f22d5debece067b9afd58c50ab46403c56b
umeme <umeme1011@gmail.com>
2023-07-28 22:52:09 +0900

    バックアップ

--- OshiDiary/ViewController/ProfileEditViewController.swift
+++ OshiDiary/ViewController/ProfileEditViewController.swift
@@ -5,4 +5,40 @@
 //  Created by umeme on 2023/07/27.
 //
 
-import Foundation
+import UIKit
+
+class ProfileEditViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // キーボード開閉時アクション設定
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+
+    }
+    
+    /**
+     キーボード表示
+     */
+    @objc func keyboardWillShow(notification: NSNotification) {
+        guard let userInfo = notification.userInfo else { return }
+        let keyboardSize = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue.height
+        scrollView.contentInset.bottom = keyboardSize
+    }
+    
+    /**
+     キーボード非表示
+     */
+    @objc func keyboardWillHide() {
+        scrollView.contentInset = .zero
+        scrollView.scrollIndicatorInsets = .zero
+    }
+
+    
+    @IBAction func tapBackBtn(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
+}
